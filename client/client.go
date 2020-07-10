1ac789631c9f1218a55046c4dd5ecfcdc0f1b88c
Frederic Jacob <fjacob@fb.com>
2020-07-10 00:00:46 -0700

    Create the import command

---
+++ client/client.go
@@ -0,0 +1,134 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"nhlpool.com/service/go/nhlpool/data"
+)
+
+// Client Object to call the service
+type Client struct {
+	server    string
+	port      int
+	url       string
+	sessionID string
+	user      string
+}
+
+// NewClient create a new client
+func NewClient(server string, port int) *Client {
+	url := fmt.Sprintf("http://%v:%v", server, port)
+	return &Client{server: server, port: port, url: url, sessionID: ""}
+}
+
+// Login using the specified credential
+func (c *Client) Login(user string, password string) error {
+	body := &data.LoginRequest{
+		Password: password,
+	}
+
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(body)
+
+	url := fmt.Sprintf("%v/player/%v/login/", c.url, user)
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	var reply data.LoginReply
+	bodyReply, _ := ioutil.ReadAll(res.Body)
+	json.Unmarshal(bodyReply, &reply)
+	if reply.Result.Code != 0 {
+		return errors.New("Cannot login")
+	}
+	c.sessionID = reply.SessionID
+	c.user = user
+	return nil
+}
+
+// Logout current session
+func (c *Client) Logout() error {
+	if c.sessionID == "" {
+		return nil
+	}
+	body := &data.LogoutRequest{
+		SessionID: c.sessionID,
+	}
+
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(body)
+
+	url := fmt.Sprintf("%v/player/%v/logout/", c.url, c.user)
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	var reply data.LogoutReply
+	bodyReply, _ := ioutil.ReadAll(res.Body)
+	json.Unmarshal(bodyReply, &reply)
+	if reply.Result.Code != 0 {
+		return errors.New("Cannot login")
+	}
+	c.sessionID = ""
+	c.user = ""
+	return nil
+}
+
+// Import a player from backup
+func (c *Client) Import(id string, player data.BackupPlayer) error {
+	if c.sessionID == "" {
+		return errors.New("Need to be logged")
+	}
+	body := data.ImportPlayerRequest{SessionID: c.sessionID}
+	body.Player.Admin = player.Admin
+	body.Player.Email = player.Email
+	body.Player.ID = id
+	body.Player.Name = player.Name
+	body.Player.Password = player.Psw
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(body)
+
+	url := fmt.Sprintf("%v/player/import/", c.url)
+	req, err := http.NewRequest("POST", url, buf)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var importReply data.ImportPlayerReply
+	bodyReply, _ := ioutil.ReadAll(res.Body)
+	json.Unmarshal(bodyReply, &importReply)
+	if importReply.Result.Code != 0 {
+		return errors.New("Cannot Import")
+	}
+	return nil
+}
